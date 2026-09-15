3e44566007515eefc123fed666b8d7378955c9da
aryansingh711 <darkdevil71029@gmail.com>
2026-09-15 09:43:21 +0530

    commit

---
+++ FSD/lab 2/index.js
@@ -0,0 +1,26 @@
+import {writeFile,readFile, appendFile} from "fs/promises";
+
+//await writeFile("stud.txt","Aryan Sharma\nRollNo:82");
+//console.log("File written");
+//const data = await readFile("stud.txt","utf-8");
+//console.log(`File contents:${data}`)
+
+const addContent=(fname,content)=>{
+     writeFile(fname,content);
+    console.log (`${content} written in file: ${fname}`);
+
+};
+
+const readContent = async (fname) => {
+    const data =await readFile(fname,'utf-8');
+    return data;
+};
+
+const appendData = async (fname,content) => {
+    await appendFile(fname,"\n" + content);
+    console.log("data appended");
+}
+await addContent ("notes.txt","FS is easy in JS");
+console.log("Contents\n",await readContent ("notes.txt"));
+await appendData("notes.txt", "it can add,read and update content");
+    console.log("Updated Contents\n", await readContent("notes.txt"));
