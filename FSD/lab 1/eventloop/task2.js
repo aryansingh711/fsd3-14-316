0b316c69727ee9408daa1e04626135a21202bb97
aryansingh711 <darkdevil71029@gmail.com>
2026-09-15 09:26:15 +0530

    commit

---
+++ FSD/lab 1/eventloop/task2.js
@@ -0,0 +1,35 @@
+import fs from "fs/promises";
+
+const writeData =async()=>{
+    try{
+        console.log("about to write....");
+        await fs.writeFile("stud.txt", "Name:Arnav Chauhan");
+        console.log("file written");
+    } catch(error){
+        console.log(error);
+    }
+}
+
+
+const f1 = () =>{
+    console.log("f1");
+};
+const f2 = () =>{
+    console.log("f2");
+};
+const f3 = () =>{
+    console.log("f3");
+};
+
+
+
+const main= () =>{
+    console.log("main");
+  setTimeout(f1, 0);
+   // setInterval(f2, 1);
+   setImmediate(f2); 
+   process.nextTick(f3);
+   writeData();
+   console.log("end");
+};
+main();
